37053ec016876be834aee7b28ffd872609df0036
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-03-31 14:55:49 +0200

    Created Point and line classes to draw lines on Canvas

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-from window import Window
+from utils import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(50,50), Point(150, 200))
+    line2 = Line(Point(0,0), Point(800, 500))
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "red")
     win.wait_for_close()
 main()
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ class Window:
         self.width = width
         self.height = height
         self.__root = Tk()
-        self.__root.title = "Maze solver"
+        self.__root.title("Maze solver")
         self.canvas = Canvas()
         self.canvas.pack()
         self.running = False
@@ -19,3 +19,22 @@ class Window:
             self.redraw()
     def close(self):
         self.running = False
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1=Point(), p2=Point()):
+        self.p1 = p1
+        self.p2 = p2
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y,
+            fill=fill_color,
+            width=2
+        )
+        canvas.pack()

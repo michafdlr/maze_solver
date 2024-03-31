4922030c0d547a1b9b794db2e394b23b5648365c
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-03-31 15:30:09 +0200

    Added cell class to draw cells

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from utils import Window, Line, Point
+from utils import Window, Line, Point, Cell
 
 def main():
     win = Window(800, 600)
-    line1 = Line(Point(50,50), Point(150, 200))
-    line2 = Line(Point(0,0), Point(800, 500))
-    win.draw_line(line1, "black")
-    win.draw_line(line2, "red")
+    # line1 = Line(Point(50,50), Point(150, 200))
+    # line2 = Line(Point(0,0), Point(800, 500))
+    # win.draw_line(line1, "black")
+    # win.draw_line(line2, "red")
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.has_right_wall = False
+    cell.draw(50,100,100,50)
     win.wait_for_close()
 main()
--- src/utils.py
+++ src/utils.py
@@ -38,3 +38,33 @@ class Line:
             width=2
         )
         canvas.pack()
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_bottom_wall = True
+        self.has_top_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(self._x1,self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(line, fill_color="black")
+        if self.has_right_wall:
+            line = Line(Point(self._x2,self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(line, fill_color="black")
+        if self.has_bottom_wall:
+            line = Line(Point(self._x1,self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(line, fill_color="black")
+        if self.has_top_wall:
+            line = Line(Point(self._x1,self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(line, fill_color="black")

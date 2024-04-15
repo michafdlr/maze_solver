7213e4880a239e8d39eccdd4a93eb5096cbc8bd0
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-04-15 18:48:19 +0200

    added timers for BFS and DFS

--- src/main.py
+++ src/main.py
@@ -1,13 +1,25 @@
 from utils import Window
 from maze_funcs import Maze
+import time
 def main():
     WIDTH, HEIGHT = 800, 600
-    num_rows = 20
-    num_cols = 20
+    num_rows = 10
+    num_cols = 10
     offset = 5
     win = Window(WIDTH, HEIGHT)
+    win.redraw()
     maze = Maze(offset,offset,num_rows,num_cols,(WIDTH-offset)//num_cols,(HEIGHT-0.5*offset)//num_rows,win)
+    t_start = time.time()
+    win.start_dfs_timer()
     maze.solve(0.1)
-    maze.solve_bfs()
+    t_end = time.time()
+    win.stop_dfs_timer()
+    print(f"Time to solve: {t_end-t_start}")
+    win.start_bfs_timer()
+    t_start = time.time()
+    maze.solve_bfs(0.1)
+    t_end = time.time()
+    win.stop_bfs_timer()
+    print(f"Time to solve: {t_end-t_start}")
     win.wait_for_close()
 main()
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, BOTH, Canvas, Button
+from tkinter import Tk, BOTH, Canvas, Button, Entry, Label
 
 class Window:
     def __init__(self, width, height):
@@ -8,19 +8,93 @@ class Window:
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+        # timer
+        self.dfs_seconds = 0
+        self.bfs_seconds = 0
+        self.dfs_timer_running = False
+        self.bfs_timer_running = False
+
+        # show time for DFS
+        self.dfs_time_label = Label(self.__root, text="Tiefensuche: 00:00", font=("Arial", 40))
+        self.dfs_time_label.pack()
+
+        # show time for BFS
+        self.bfs_time_label = Label(self.__root, text="Breitensuche: 00:00", font=("Arial", 40))
+        self.bfs_time_label.pack()
+
+        # start and stop buttons
+        # self.start_button = Button(self.__root, text="Start", command=self.start_timer)
+        # self.start_button.pack()
+        # self.stop_button = Button(self.__root, text="Stop", command=self.stop_timer)
+        # self.stop_button.pack()
+
+        #self.update_timer()
+
+    def start_dfs_timer(self):
+        if not self.dfs_timer_running:
+            self.dfs_timer_running = True
+            self.update_timer(True)
+
+    def stop_dfs_timer(self):
+        self.dfs_timer_running = False
+        self.dfs_seconds = 0
+
+    def start_bfs_timer(self):
+        if not self.bfs_timer_running:
+            self.bfs_timer_running = True
+            self.update_timer(False)
+
+    def stop_bfs_timer(self):
+        self.bfs_timer_running = False
+        self.bfs_seconds = 0
+
+    def update_timer(self, dfs=True):
+        if dfs:
+            seconds = self.dfs_seconds
+            label = self.dfs_time_label
+            timer_running = self.dfs_timer_running
+        else:
+            seconds = self.bfs_seconds
+            label = self.bfs_time_label
+            timer_running = self.bfs_timer_running
+
+        if timer_running:
+            seconds += 1
+            if dfs:
+                self.dfs_seconds = seconds
+                label.config(text=f"Tiefensuche: {seconds//60:02d}:{seconds%60:02d}")
+            else:
+                self.bfs_seconds = seconds
+                label.config(text=f"Breitensuche: {seconds//60:02d}:{seconds%60:02d}")
+            self.__root.after(1000, self.update_timer, dfs)
+
+
+        #self.create_restart_button()
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
+
     def wait_for_close(self):
         self.__running = True
         while self.__running:
             self.redraw()
         print("Window closed by user!")
+
+    # def create_restart_button(self):
+    #     self.__quit_button = Button(self.__root, text="Neustart", command=self.restart)
+    #     self.__quit_button.pack()
+
+    # def restart(self):
+    #     self.__running = False
+    #     self.__root.destroy()
+
+
     def close(self):
         self.__running = False
+
     def draw_line(self, line, fill_color):
         line.draw(self.__canvas, fill_color)
-
 
 class Point:
     def __init__(self, x=0, y=0):
